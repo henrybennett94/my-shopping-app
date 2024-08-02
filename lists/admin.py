1e1e516199b62439dc3d9fa796daff7cd0370a7c
Henry Bennet <hwbennett94@gmail.com>
2024-08-02 13:29:14 +0000

    Create open_shopping_list function

--- lists/admin.py
+++ lists/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.ShoppingList)
+admin.site.register(models.List)
+admin.site.register(models.Items)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from .models import List, Items
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -8,7 +12,19 @@ from django.views.generic import TemplateView
 #    Displays home page" """
 #    template_name = 'index.html'
 
-def shopping_list_view(request):
-    return render(request, 'shopping_list.html', {'items': GroceryItem.all()})
-
+@login_required(login_url='/login/')
+def create_shopping_list(request):
+    user = request.user
+    if request.method == 'POST':
+        if request.POST.get("new_list"):
+            list_name = request.POST.get("list_name")
+@login_required(login_url='/login/')
+def open_shopping_list(request, list_name):
+    queryset = List.objects.filter(status=1)
+    list = get_object_or404(queryset, list_name=list_name)
+    return render(
+        request,
+        "lists/templates/lists/shopping_list.html",
+        {"list": list},
+    )
 
