91f669ed596a94006b0ccec7bddcb20d583b9f49
Henry Bennet <hwbennett94@gmail.com>
2024-08-05 09:05:52 +0000

    Set up html templates in lists app

--- lists/urls.py
+++ lists/urls.py
@@ -5,8 +5,7 @@ from .views import create_shopping_list, open_shopping_list, add_item
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('', include('lists.urls'), name='lists-urls'),
     path('create/', create_shopping_list, name='create_shopping_list'),
-    path('list/<int:id>/', open_shopping_list, name='open_shopping_list'),
+    path('shopping_list/<int:id>/', open_shopping_list, name='open_shopping_list'),
 ]
 
--- lists/views.py
+++ lists/views.py
@@ -8,9 +8,9 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
-#class HomePage(TemplateView):
-#    Displays home page" """
-#    template_name = 'index.html'
+class HomePage(TemplateView):
+    """Displays home page """
+    template_name = 'index.html'
 
 @login_required(login_url='/login/')
 def create_shopping_list(request):
@@ -23,12 +23,12 @@ def create_shopping_list(request):
             shopping_list.user = request.user
             shopping_list.save()
 
-            return redirect('shopping_list.html', id=shopping_list.id)
+            return redirect('new_list.html', id=shopping_list.id)
         
         else:
             list_form=ListForm()
         
-        return render(request, 'shopping_list.html', {
+        return render(request, 'new_list.html', {
             'list_form': list_form
         })
 
@@ -37,7 +37,7 @@ def open_shopping_list(request, id):
     shopping_list = get_object_or404(List, id=id)
     items = shopping_list.items.all()
   #  return render(request, "lists/templates/lists/shopping_list.html",{"list": list},)
-    return render(request, 'shopping_list.html', {'shopping_list': shopping_list, 'items': items})
+    return render(request, 'list_detail.html', {'shopping_list': shopping_list, 'items': items})
 
 @login_required(login_url='/login/')
 def add_item(request):
@@ -48,11 +48,11 @@ def add_item(request):
         item.list = shopping_list
         item.save()
 
-        return redirect('shopping_list.html', id=shopping_list.id)
+        return redirect('list_detail.html', id=shopping_list.id)
     
     else:
         add_item_form = AddItemForm()
     
-    return render(request, 'shopping_list.html', {
+    return render(request, 'list_detail.html', {
         'add_item_form': add_item_form
     })
